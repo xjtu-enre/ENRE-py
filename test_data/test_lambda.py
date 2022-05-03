fb5c11e23a01d0afe0ef8c452a5baf38e5fb048b
ZDH <935021934@qq.com>
2022-05-03 20:06:30 +0800

    Use dependencies to outer scope

--- test_data/test_lambda.py
+++ test_data/test_lambda.py
@@ -14,6 +14,26 @@ l1 = lambda a: [y + 1 for y in l]
 # D: Define-$anonymous1->$y@y in
 # D: Use-$anonymous1->$y@y + 1
 
+l2 = lambda a: {y + 1 for y in l}
+# E: AnonymousFunction-$anonymous2=test_lambda.($line)@lambda
+# E: Variable-$l2=test_lambda.l2@l2
+# E: Lambda Parameter-$pa=test_lambda.($line).a@a:
+# E: Variable-$y=test_lambda.($line).y@y in
+# D: Define-$tl->$l2@l1
+# D: Define-$anonymous2->$pa@a:
+# D: Define-$anonymous2->$y@y in
+# D: Use-$anonymous2->$y@y + 1
+
+l3 = lambda a: (y + 1 for y in l)
+# E: AnonymousFunction-$anonymous3=test_lambda.($line)@lambda
+# E: Variable-$l3=test_lambda.l3@l3
+# E: Lambda Parameter-$pa=test_lambda.($line).a@a:
+# E: Variable-$y=test_lambda.($line).y@y in
+# D: Define-$tl->$l1@l1
+# D: Define-$anonymous3->$pa@a:
+# D: Define-$anonymous3->$y@y in
+# D: Use-$anonymous3->$y@y + 1
+
 def wrapper():
     # E: Function-$wrapper=test_lambda.wrapper@wrapper
     # D: Define-$tl->$wrapper@wrapper
--- test_data/test_variable.py
+++ test_data/test_variable.py
@@ -19,9 +19,18 @@ t1, t2 = 1, 2
 # E: Variable-$t3=test_variable.t3@t3
 # D: Define-$tv->$t3@t3
 
-def func():
+global_var = 1
+# E: Variable-$global_var=test_variable.global_var@global_var
+# D: Define-$tv->$global_var@global_var
+
+
+def func(p1, p2):
     # E: Function-$f=test_variable.func@def func
+    # E: Parameter-$p1=test_variable.func.p1@p1
+    # E: Parameter-$p2=test_variable.func.p2@p2
     # D: Define-$tv->$f@def func
+    # D: Define-$f->$p1@p1
+    # D: Define-$f->$p2@p2
     x = 1
     # E: Variable-$x1=test_variable.func.x@x
     # D: Define-$f->$x1@x
@@ -39,5 +48,24 @@ def func():
     (t3 := 1)
     # E: Variable-$ft3=test_variable.func.t3@t3
     # D: Define-$f->$ft3@t3
+
+    if p1 > p2:
+        x = x - 1
+        # D: Use-$f->$x1@x - 1
+        # D: Set-$f->$x1@x =
+
+    for a, b in p1:
+        # E: Variable-$a=test_variable.func.a@a, b
+        # E: Variable-$b=test_variable.func.b@b in
+        # E: Use-$f->$p1@p1
+        func(a, b)
+        # D: Call-$f->$f@func
+        # D: Use-$f->$a@a
+        # D: Use-$f->$b@b
+
+        func(t1, global_var)
+        # D: Call-$f->$f@func
+        # D: Use-$f->$ft1@t1
+        # D: Use-$f->$global_var@global_var
 
 
