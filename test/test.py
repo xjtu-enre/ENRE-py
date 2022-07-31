30458a8020fc760b7042881507d1d06cadc13875
ZDH <935021934@qq.com>
2022-07-31 22:34:14 +0800

    test example

--- test/test.py
+++ test/test.py
@@ -1,6 +1,48 @@
-import und_entry
-
-
 def func3(x, *y, **z):
     ...
 
+
+def bar():
+    ...
+
+
+def foo():
+    ...
+
+
+def identity(x):
+    return x
+
+
+if ...:
+    f1 = identity(identity(identity(foo)))
+else:
+    def f1():
+        ...
+
+f1()
+
+
+def create_class():
+    class Difficult:
+        def test(self):
+            ...
+
+    return Difficult
+
+
+cls = create_class()
+
+difficult_obj = cls()
+
+difficult_obj.test()
+
+
+class A():
+    def test(self):
+        ...
+
+
+a = A()
+
+a.test()
